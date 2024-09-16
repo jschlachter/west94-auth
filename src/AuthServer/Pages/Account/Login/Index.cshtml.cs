4abb8f016c8a292df2b1663fcdc20f253de1d51d
Jeremy Schlachter <jeremy.schlachter@west94.org>
2024-09-15 19:09:13 -0500

    Account login

--- src/AuthServer/Pages/Account/Login/Index.cshtml.cs
+++ src/AuthServer/Pages/Account/Login/Index.cshtml.cs
@@ -1,15 +1,21 @@
 using System.Collections.Immutable;
+using System.Security.Claims;
 
 using AuthServer.Domain.AggregatesModel.ApplicationUserAggregate;
 
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Http.Diagnostics;
+using Microsoft.Identity.Client.TelemetryCore.TelemetryClient;
+using Microsoft.IdentityModel.Tokens;
 
 using OpenIddict.Abstractions;
+using OpenIddict.Client.AspNetCore;
 using OpenIddict.EntityFrameworkCore.Models;
 
 using static OpenIddict.Abstractions.OpenIddictConstants;
@@ -49,6 +55,48 @@ public class Index : PageModel
             return RedirectToPage("/ExternalLogin/Challange", new { scheme = View.ExternalLoginScheme, returnUrl});
         }
 
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPost()
+    {
+        var request = HttpContext.GetOpenIddictClientRequest();
+
+        if (Input.Button != "login") {
+            // if the user cancels, send a result back into IdentityServer as if they 
+            // denied the consent (even if this client does not require consent).
+            // this will send back an access denied OIDC error response to the client.
+
+            return RedirectToPage("~/");
+        }
+
+        if (ModelState.IsValid) 
+        {
+            var result = await _signInManager.PasswordSignInAsync(Input.Username!, Input.Password!, Input.RememberLogin, lockoutOnFailure: true);
+            if (result.Succeeded)
+            {
+                var user = await _userManager.FindByNameAsync(Input.Username!);
+                
+                var identity = new ClaimsIdentity(CookieAuthenticationDefaults.AuthenticationScheme);
+
+                identity.SetClaim(ClaimTypes.Email, user!.Email);
+                identity.SetClaim(ClaimTypes.Name, user!.UserName);
+                identity.SetClaim(ClaimTypes.NameIdentifier, user!.Id);
+                
+                var properties = new AuthenticationProperties
+                {
+                    RedirectUri = Input!.ReturnUrl
+                };
+
+                await _signInManager.SignInAsync(user, Input.RememberLogin, CookieAuthenticationDefaults.AuthenticationScheme);
+                
+                return SignIn(new ClaimsPrincipal(identity), properties, CookieAuthenticationDefaults.AuthenticationScheme);
+            }
+
+            ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+        }
+
+        await BuildModelAsync(Input.ReturnUrl);
         return Page();
     }
 
